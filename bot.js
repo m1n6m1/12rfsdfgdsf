976edb0aa619a9b12e41b5313a62b0204a4befff
Noel <m1n6m1@users.noreply.github.com>
2018-01-19 18:00:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,21 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+(function($) {
+    $.rand = function(arg) {
+        if ($.isArray(arg)) {
+            return arg[$.rand(arg.length)];
+        } else if (typeof arg == "number") {
+            return Math.floor(Math.random() * arg);
+        } else {
+            return 4;  // chosen by fair dice roll
+        }
+    };
+})(jQuery);
+
+var items = ['https://www.picstatio.com/large/fe36b4/Kumiko-Oumae-Reina-Kousaka-anime-girls.jpg','https://i.imgur.com/ymG5bnZ.jpg'];
+var item = jQuery.rand(items);
+
 client.on('message', message => {
     if (message.content === '&fuck') {
     	message.channel.send('https://78.media.tumblr.com/f1ace6e731fbfc240d89690911dd2d9f/tumblr_n8uqbrSpFZ1tp5eoko4_500.gif');
@@ -13,7 +28,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '&kumiko') {
-    	message.channel.send(var randomArray = ['https://i.imgur.com/ymG5bnZ.jpg','https://www.picstatio.com/large/fe36b4/Kumiko-Oumae-Reina-Kousaka-anime-girls.jpg', 'I died for kumiko btw'];);
+    	message.channel.send(var item);
   	}
 });
 
