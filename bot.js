5b47744b432d7dd51a6dd46d01c2a6f3af13425e
M1n6m1 <m1n6m1@gmail.com>
2018-01-19 21:27:20 +0100

    Added prefix & better sintax & disabled status

--- bot.js
+++ bot.js
@@ -1,31 +1,42 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = "!";
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('ready', () => {
-  bot.user.setGame('I <3 Nax')
-})
+//client.on('ready', () => {
+//  bot.user.setGame('I <3 Nax')
+//})
 
 client.on('message', message => {
-    if (message.content === '&fuck') {
+    if (message.content.startsWith(prefix + 'fuck')) {
     	message.channel.send('https://78.media.tumblr.com/f1ace6e731fbfc240d89690911dd2d9f/tumblr_n8uqbrSpFZ1tp5eoko4_500.gif');
   	}
 });
 
-client.on('message', message => {
-    if (message.content === 'ayy') {
-    	message.channel.send('lmao');
-  	}
-});
 
 client.on('message', message => {
     if (message.content === '&help') {
     	message.reply('There is no help sorry :<');
   	}
 });
+
+// No prefix responds
+client.on("message", (message) => {
+  if(responseObject[message.content]) {
+    message.channel.send(responseObject[message.content]);
+  }
+});
+
+const responseObject = {
+  "ayy": "Ayy, lmao!",
+  "wat": "Say what?",
+  "lol": "roflmaotntpmp"
+};
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
