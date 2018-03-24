d8f372a432277824568b370f05e3ec8371afe137
Joel Sifres <joelsifresclemente@gmail.com>
2018-03-24 21:24:27 +0100

    añadir comando "a"
    :)

--- bot.js
+++ bot.js
@@ -48,17 +48,18 @@ const responseObject = {
 	"hitler" : "mechahitler",
 	"stalin" : "mechastalin",
 	"asuka": "puta",
-"infierno": "dakara hashirou ze hashirou ze" +
-"kikasete　omae no koe" +
-"dare yori mo subete o fukaku fukaku kanjitai" +
-"tomaranai tomaranai" +
-"hitori ja nai" +
-"(Get Our Life!) omae no omoi 　 (Get Our Dream!) mune ni daite" +
-"hashiri tsudzukeru",
-"ranmaru": "Banana is PERFECT!",
-"capi": "https://gyazo.com/38d8417d1c1ef4cdabbd5b1c7bb72a45",
-"reiji": "yoroshiku machocho",
-};
+  "infierno": "dakara hashirou ze hashirou ze" +
+  "kikasete　omae no koe" +
+  "dare yori mo subete o fukaku fukaku kanjitai" +
+  "tomaranai tomaranai" +
+  "hitori ja nai" +
+  "(Get Our Life!) omae no omoi 　 (Get Our Dream!) mune ni daite" +
+  "hashiri tsudzukeru",
+  "ranmaru": "Banana is PERFECT!",
+  "capi": "https://gyazo.com/38d8417d1c1ef4cdabbd5b1c7bb72a45",
+  "reiji": "yoroshiku machocho",
+  "a": "aaaaaaaaaaaaaaaa",
+  };
 
 
 // THIS  MUST  BE  THIS  WAY
