2e82fdfbfc9d086da4a09126b4bf81ff186cf719
M1n6m1 <m1n6m1@gmail.com>
2018-03-24 20:46:45 +0100

    More cmds

--- bot.js
+++ bot.js
@@ -39,7 +39,24 @@ const responseObject = {
   "nepe": "pene",
   "ur mom gay": "no you",
   "no u": "no u",
-  "kys": "https://imgur.com/TEqfNbQ"
+  "kys": "https://imgur.com/TEqfNbQ",
+  "best boi": 'sesil',
+  "jav": "My Mother Saw Everything! An 8 Hour Special This Mother Accidentally Caught Her Son Masturbating, And Although She Knew It Was Wrong, She Was An Overprotective Mom Who Felt That She Had To Help, And Ended Up Allowing Him To Insert His Cock Into Her Willing Pussy...",
+  "100tifico": "pa k kieres saber eso jaja salu2",
+  "debiruman": "Are wa dare da dare da dare da Are wa debiru Debiruman Debiruman",
+  "joel becoming" : "a dj",
+	"hitler" : "mechahitler",
+	"stalin" : "mechastalin",
+	"asuka": "puta",
+"infierno": "dakara hashirou ze hashirou ze" +
+"kikasete　omae no koe" +
+"dare yori mo subete o fukaku fukaku kanjitai" +
+"tomaranai tomaranai" +
+"hitori ja nai" +
+"(Get Our Life!) omae no omoi 　 (Get Our Dream!) mune ni daite" +
+"hashiri tsudzukeru",
+"ranmaru": "Banana is PERFECT!",
+"capi": "https://gyazo.com/38d8417d1c1ef4cdabbd5b1c7bb72a45",
 };
 
 
