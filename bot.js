7f75040b328b729e84580ba93972eac86828f213
M1n6m1 <m1n6m1@gmail.com>
2018-03-26 15:36:49 +0200

    nvm

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ const responseObject = {
   "ayy": "Ayy, lmao!",
   "eba": "https://cdn.discordapp.com/attachments/427167187120160768/427174267210301451/magik.png",
   "nepe": "pene",
-  "ur mom gay": "no u",
+  "ur mom gay": "no you",
   "no u": "no u",
   "kys": "https://imgur.com/TEqfNbQ",
   "best boi": 'sesil',
